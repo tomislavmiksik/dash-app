ea7f4a2fb9e8fac6da85738aa28e4bca300ffeeb
Tomislav Mikšik <64797760+tomislavmiksik@users.noreply.github.com>
2022-01-20 22:33:38 +0100

    init commit

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,34 @@
+import 'package:dash_app/screens/weather_screen.dart';
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({ Key? key }) : super(key: key);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedIndex = 0;
+  late List<dynamic> pages = [];
+  late List<dynamic> appBars = [];
+
+   @override
+  void initState() {
+    pages = [
+      WeatherScreen(),
+    ];
+    appBars = [
+
+    ];
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
